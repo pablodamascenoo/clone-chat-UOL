fcefaa5f595b94b536e83370995627bdd3fc7c20
pablodamascenoo <pablofacex@gmail.com>
2022-02-09 21:54:14 -0300

    change printMessage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,18 +27,21 @@ function printAllMessages(obj){
 
 function printMessage(content){
     
-    let message = document.createElement("div")
-    message.classList.add("message-display", content.type)
-    message.innerHTML = `<p class="time">(${content.time})</p>`
-    if(content.type !== "status" ){
-        message.innerHTML += `<h2 class="name">${content.from} <span>to</span> ${content.to}: </h2>`
+    if(!(content.type === "private_message" && content.to !== user)){
+        let message = document.createElement("div")
+        message.classList.add("message-display", content.type)
+        message.innerHTML = `<p class="time">(${content.time})</p>`
+        if(content.type !== "status" ){
+            message.innerHTML += `<h2 class="name">${content.from} <span>to</span> ${content.to}: </h2>`
+        }
+        else{
+            message.innerHTML += `<h2 class="name">${content.from}</h2>`
+        }
+        message.innerHTML += `<p>${content.text}</p>`
+        message.scrollIntoView()
+        document.querySelector(".messages-box").appendChild(message)
     }
-    else{
-        message.innerHTML += `<h2 class="name">${content.from}</h2>`
-    }
-    message.innerHTML += `<p>${content.text}</p>`
-    message.scrollIntoView()
-    document.querySelector(".messages-box").appendChild(message)
+    
 }
 
 function refreshLogIn(){
