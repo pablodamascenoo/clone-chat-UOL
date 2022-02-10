72855f1c21b34a05241d03d768a41f6d951de0e3
pablodamascenoo <pablofacex@gmail.com>
2022-02-10 16:54:26 -0300

    enabled private messages

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,7 @@ function refreshMessages(){
 
     const promisse = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages")
     promisse.then(printAllMessages)
+    reloadParticipants()
 }
 
 function printAllMessages(obj){
@@ -27,7 +28,7 @@ function printAllMessages(obj){
 
 function printMessage(content){
     
-    if(!(content.type === "private_message" && content.to !== user)){
+    if(!(content.type === "private_message" && content.to !== user && content.from !== user)){
         let message = document.createElement("div")
         message.classList.add("message-display", content.type)
         message.innerHTML = `<p class="time">(${content.time})</p>`
@@ -80,11 +81,21 @@ function sendMessage(){
 
     let message = message_area.value
 
+    let type = ""
+
+    if(options.visibility === "Público"){
+        type = "message"
+    }
+
+    else{
+        type = "private_message"
+    }
+
     let obj = {
         from: user,
-        to: "Todos",
+        to: options.people,
         text: message,
-        type: "message" // ou "private_message" para o bônus
+        type: type // ou "private_message" para o bônus
     }
 
     let promisse = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", obj)
@@ -110,7 +121,7 @@ message_area.addEventListener("keypress", (e)=>{
 function toggleCheck(obj){
 
     if(obj.parentNode.classList.contains("people")){
-        if(options.people !== ""){
+        if(obj.parentNode.querySelector(".check") !== null){
             obj.parentNode.querySelector(".check").classList.remove("check")
             obj.lastElementChild.classList.add("check")
             options.people = obj.querySelector("div > p").innerText
@@ -134,5 +145,39 @@ function toggleCheck(obj){
 }
 
 function toggleAside(){
+    reloadParticipants()
     document.querySelector("aside").classList.toggle("disabled")
+}
+
+function reloadParticipants(){
+
+    let promisse = axios.get("https://mock-api.driven.com.br/api/v4/uol/participants")
+
+    promisse.then((obj)=>{
+
+        let participants = document.querySelector(".people")
+
+        participants.innerHTML = `
+        <div class="option" onclick="toggleCheck(this)">
+            <div>
+                <ion-icon name="people"></ion-icon>
+                <p>Todos</p>
+            </div>
+            <ion-icon name="checkmark"></ion-icon>
+        </div>`
+
+        data = obj.data
+
+        for(let i=0; i<data.length; i++){
+            participants.innerHTML += `
+            <div class="option" onclick="toggleCheck(this)">
+                <div>
+                    <ion-icon name="person-circle"></ion-icon>
+                    <p>${data[i].name}</p>
+                </div>
+                <ion-icon name="checkmark"></ion-icon>
+            </div>`
+        }
+    })
+
 }
